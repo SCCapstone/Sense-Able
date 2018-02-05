4afe8c769e0e96c44175ec3b4d4009c7026d7c3c
TheIndexZero <cckisby@gmail.com>
2018-02-04 19:26:26 -0500

    Documented a little

--- leddargui/QtBehaviorTestSuite.cpp
+++ leddargui/QtBehaviorTestSuite.cpp
@@ -12,12 +12,15 @@ void QtBehaviorTestSuite::runTests() {
 
 void QtBehaviorTestSuite::testStreamButtonClicked() {
     MainWindow window;
+
+    // Set up a spy to check whether the Stream Button has been clicked.
     QSignalSpy spy (&window, SIGNAL(streamButtonClicked()));
 
     // Simulate clicking the Stream Button.
     QWidget *button = window.findChild<QPushButton*>("streamButton");
     //          HOW DO WE GET THE streamButton?????
     QTest::mouseClick(button, Qt::LeftButton, Qt::NoModifier);
+
 
     cout << "testStreamButtonClicked:  ";
     if (spy.count() == 1) {
